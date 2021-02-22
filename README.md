50a68d205750bc33d44ff55d4c65de31eb21d51f
Calx <robuk@bokuno.com>
2021-02-23 01:34:24 +0900

    add: first commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # random-string
+
+This repository generate a random string.
+
+## Dependency
+
+* python 3.9.0
+
+## Setup
+
+1. Homebrew install
+2. `brew install anyenv`
+
+## Usage
+
+## Demo
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="rndstr",
+    version='1.0',
+    description='a random string generate',
+    author='calx0209',
+    url='https://github.com/calx0209/random-string',
+)
