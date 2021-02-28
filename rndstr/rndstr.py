3f07257315390a82b28bd1e184fcdc5845500b7a
Calx <robuk@bokuno.com>
2021-03-01 00:48:21 +0900

    add: random string 8 characters

--- rndstr/rndstr.py
+++ rndstr/rndstr.py
@@ -1,2 +1,4 @@
-def hello(name):
-    print('Hello, %s!' % name)
+import random, string
+
+def rndstr(output_number):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=output_number))
--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="rndstr",
-    version='1.0',
+    version='1.1',
     description='a random string generate',
     author='calx0209',
     url='https://github.com/calx0209/random-string',
+    pachage=find_packages(),
+    entry_points={
+        "console_scripts":[
+            "randomstring = rndstr.app:main",
+        ]
+    },
 )
